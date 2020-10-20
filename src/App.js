87ac295f171fd16eb7fcb79a87ec3468857288fd
Steven Polido <steven.polido@vistage.com>
2020-10-20 15:16:03 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import plaza_1 from './plaza1.gif';
+import plaza_2 from './plaza2.gif'
 import './App.css';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  const handleClickCount = (e) => {
+    setCounter(prevCount => prevCount + 1);
+  }
+
+  if(counter === 3) {
+    console.log('count reached three clicks');
+  }
+
+  console.log(counter)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <p>Aubrey was clicked { counter } times.</p>
+      { counter === 3  ? (
+        <img src={ plaza_1 } alt="aubrey plaza" />
+      ) : (
+        <img src={ plaza_2 } onClick={ handleClickCount } alt="aubrey plaza" />
+      ) }
     </div>
   );
 }
